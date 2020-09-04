66800bad44a6ce318726ca2bb242fc399d1db0e5
cakirefekan <cakirefekan@gmail.com>
2020-09-04 18:05:26 +0300

    Update json.js

--- istanbulBikeMaps/json.js
+++ istanbulBikeMaps/json.js
@@ -1,16 +1,26 @@
-var xhttp = new XMLHttpRequest()
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    //console.log(this.responseText)
-  }
-}
-xhttp.open(
-  'POST',
-  'https://api.ibb.gov.tr/ispark-bike/GetAllStationStatus',
-  true
-)
+/*var xhttp = new XMLHttpRequest()
+xhttp.open('POST', 'http://temiz.info/', true)
+xhttp.withCredentials = true
+xhttp.setRequestHeader('Content-Type', 'application/json')
 xhttp.onload = function () {
   var data = JSON.parse(this.response)
   console.log(data)
 }
-xhttp.send()
+xhttp.send({ request: 'authentication token' })
+*/
+async function get() {
+  let url = 'data.js'
+  let response = await fetch(url, {
+    method: 'POST'
+  })
+
+  if (response.ok) {
+    // if HTTP-status is 200-299
+    // get the response body (the method explained below)
+    let json = await response.json()
+    console.log(json)
+  } else {
+    alert('HTTP-Error: ' + response.status)
+  }
+}
+get()
