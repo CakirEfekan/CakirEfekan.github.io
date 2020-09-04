ad5180fb1fe2490e6998818a648b004a81ba7b3c
cakirefekan <cakirefekan@gmail.com>
2020-09-04 18:50:46 +0300

    Update json.js

--- istanbulBikeMaps/json.js
+++ istanbulBikeMaps/json.js
@@ -8,26 +8,3 @@ xhttp.onload = function () {
 }
 xhttp.send({ request: 'authentication token' })
 */
-async function get() {
-  let url = 'data.json'
-  let response = await fetch(url)
-
-  if (response.ok) {
-    // if HTTP-status is 200-299
-    // get the response body (the method explained below)
-    let json = await response.json()
-    let stations = json.dataList
-    stations.map(
-      (station) =>
-        function () {
-          console.log(station.lat)
-          L.marker([station.lat, station.lon])
-            .bindPopup('I am a green leaf.')
-            .addTo(map)
-        }
-    )
-  } else {
-    // alert('HTTP-Error: ' + response.status)
-  }
-}
-get()
