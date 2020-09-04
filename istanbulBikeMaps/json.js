46a285b2c904ebed05bc98154fe4e58aff0048e8
cakirefekan <cakirefekan@gmail.com>
2020-09-04 17:35:32 +0300

    Update json.js

--- istanbulBikeMaps/json.js
+++ istanbulBikeMaps/json.js
@@ -1,7 +1,7 @@
 var xhttp = new XMLHttpRequest()
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText)
+    //console.log(this.responseText)
   }
 }
 xhttp.open(
@@ -9,4 +9,8 @@ xhttp.open(
   'https://api.ibb.gov.tr/ispark-bike/GetAllStationStatus',
   true
 )
+xhttp.onload = function () {
+  var data = JSON.parse(this.response)
+  console.log(data)
+}
 xhttp.send()
