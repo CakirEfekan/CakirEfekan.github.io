8ea3cf3123eaa7082c79f8aaf0a17b583b5f3bcb
cakirefekan <cakirefekan@gmail.com>
2020-09-05 16:03:00 +0300

    Update kmlLoader.js

--- istanbulBikeMaps/kmlLoader.js
+++ istanbulBikeMaps/kmlLoader.js
@@ -11,3 +11,16 @@ fetch('mevcut-bisiklet-yollar.kml')
     const bounds = track.getBounds()
     map.fitBounds(bounds)
   })
+fetch('paylaml-bisiklet-yollar.kml')
+  .then((res) => res.text())
+  .then((kmltext) => {
+    // Create new kml overlay
+    const parser = new DOMParser()
+    const kml = parser.parseFromString(kmltext, 'text/xml')
+    const track = new L.KML(kml)
+    map.addLayer(track)
+
+    // Adjust map to show the kml
+    const bounds = track.getBounds()
+    map.fitBounds(bounds)
+  })
