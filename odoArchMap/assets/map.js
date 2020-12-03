aa8db1c447d32730bfdc5dd6877a23c9b8ec2a89
Efekan ÇAKIR <cakirefekan@gmail.com>
2020-12-03 21:19:03 +0300

    ready for first publish

--- odoArchMap/assets/map.js
+++ odoArchMap/assets/map.js
@@ -1,9 +1,15 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+let zoom = 7,
+    centeredLatlong = [38.963745, 35.243322]
+var options = {
+    attribution: '&copy; <a target="_blank" href="https://www.mapbox.com/feedback/">Mapbox</a> <a target="_blank" href="http://openstreetmap.org/copyright">&copy; OpenStreetmap</a> | Projeler <a target="_blank" href="https://odoarchitecture.com/">One Day One Architecture</a> | Geliştirici <a target="_blank" href="https://efekan.kim/">Efekan</a>',
+    subdomains: 'abc',
+    minZoom: 0,
+    maxZoom: 20
+}
+mapboxSatelliteUrl = "https://api.mapbox.com/styles/v1/cakirefekan/ckhxfelen0e7119lfxil6ifch/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY2FraXJlZmVrYW4iLCJhIjoiY2tlcHAzZWNtMmppdDJzcGMxOG04a21kMyJ9._Z_qxs1w7c87CsBWii_A8w",
+    mapboxSatellite = L.tileLayer(mapboxSatelliteUrl, options)
+map = L.map('map', {
+    layers: [mapboxSatellite],
+    center: centeredLatlong,
+    zoom: zoom
+})
---
+++ odoArchMap/assets/markers.js
@@ -0,0 +1,284 @@
+let projectArray = [{
+        "details": {
+            "name": "Çukurova Belediyesi ve Orhan Kemal Kültür Merkezi",
+            "latLong": [37.04009991562154, 35.271496330846794],
+            "arch": ["MTF Proje"],
+            "year": "2011-2013",
+            "webpage": "http://www.mtfproje.com.tr/portfolios/cukurova-ilce-belediyesi-hizmet-binasi-ve-kultur-merkezi/"
+        }
+    },
+    {
+        "details": {
+            "name": "ODTÜ KKK Mühendislik Laboratuvarları",
+            "latLong": [35.24985346947768, 33.02105614684695],
+            "arch": ["FREA", "ASMA"],
+            "year": "2014",
+            "webpage": "http://frea.com.tr/TR/Projects/41/Selected/268/Metu-Ncc/"
+        }
+    },
+    {
+        "details": {
+            "name": "Kadirli Belediyesi Hizmet Binası ve Kültür Merkezi",
+            "latLong": [37.368182881369314, 36.09440208288181],
+            "arch": ["İkiartıbir Mimarlık"],
+            "year": "2009",
+            "webpage": "http://www.ikiartibir.com.tr/prizes_detail.php?id=12"
+        }
+    },
+    {
+        "details": {
+            "name": "Konyaaltı Kentsel Tasarım ve Kıyı Düzenlemesi",
+            "latLong": [36.86653800061519, 30.644140649091444],
+            "arch": ["Özer/Ürger Mimarlık"],
+            "year": "2014-2017",
+            "webpage": "http://ozerurger.com/portfolio-item/konyaalti-urban-design-and-coastal-regeneration/?lang=tr"
+        }
+    },
+    {
+        "details": {
+            "name": "Yeşilova Höyüğü Ziyaretçi Merkezi",
+            "latLong": [38.44105105339826, 27.213553040576176],
+            "arch": ["Studio Evren Başbuğ", "SCRA Mimarlık"],
+            "year": "2010-2011",
+            "webpage": "https://www.studioevrenbasbug.com/post/yesilova-hoyuk-visitor-center"
+        }
+    },
+    {
+        "details": {
+            "name": "Antalya Şehirlerarası Otobüs Terminali",
+            "latLong": [36.9222058846115, 30.665442237881813],
+            "arch": ["Hazan Mimarlık"],
+            "year": "1991-1997",
+            "webpage": "http://www.hazanmimarlik.com/project/antalya-bus-terminal-1"
+        }
+    },
+    {
+        "details": {
+            "name": "İAOSB Nedim Uysal Mesleki ve Teknik Anadolu Lisesi",
+            "latLong": [38.50666532163729, 27.037268882616992],
+            "arch": ["M+D Mimarlık"],
+            "year": "2012-2016",
+            "webpage": "http://www.martid.com/tr/projects-detail/iaosb-lisesi"
+        }
+    },
+    {
+        "details": {
+            "name": "Kara Harp Okulu Laboratuvar Merkezi",
+            "latLong": [39.907838369478256, 32.841857858175125],
+            "arch": ["FREA", "SCRA Mimarlık"],
+            "year": "2013",
+            "webpage": "http://frea.com.tr/TR/Projects/42/Tumu/13/KARA-HARP-OKULU-LABORATUVAR-MERKEZi/"
+        }
+    },
+    {
+        "details": {
+            "name": "Kemal Kurdaş Anıt Mezarı",
+            "latLong": [41.172487079655895, 29.054282279770774],
+            "arch": ["ASMA", "MDesign"],
+            "year": "2013",
+            "webpage": "https://www.mdproje.com/portfolio/kemal-kurdas-mezar-yarismasi/"
+        }
+    },
+    {
+        "details": {
+            "name": "Adnan Menderes Dış Hatlar Terminali",
+            "latLong": [38.29392087382268, 27.15397129586015],
+            "arch": ["Hazan Mimarlık"],
+            "year": "1998-2012",
+            "webpage": "http://www.hazanmimarlik.com/tr/project/adnan-menderes-havaalani-dis-hatlar-terminali"
+        }
+    },
+    {
+        "details": {
+            "name": "A.Ü. Yabancı Diller Yüksekokulu",
+            "latLong": [39.816333886874055, 30.53078189082373],
+            "arch": ["SE Mimarlık"],
+            "year": "2007-2009",
+            "webpage": "https://semim.com/proje/034-anadolu-universitesi-yabanci-diller-yuksek-okulu/"
+        }
+    },
+    {
+        "details": {
+            "name": "Ayhan Şahenk Tarım Bilimleri ve Teknolojileri Fakültesi",
+            "latLong": [37.94372715543453, 34.62806601329182],
+            "arch": ["PAB Mimarlık"],
+            "year": "2012",
+            "webpage": "https://www.pab.com.tr/ayhan-sahenk-tarim-bilim-fakultesi"
+        }
+    },
+    {
+        "details": {
+            "name": "Teknopark İstanbul: İstanbul Teknoloji Geliştirme Bölgesi Yönetim ve Ar-Ge Yapısı",
+            "latLong": [40.92152350464629, 29.31549781489294],
+            "arch": ["Nurbin Paker", "Hüseyin Kahvecioğlu", "Cem Altun", "Melis Nur İhtiyar Varol"],
+            "year": "2012-2017",
+            "webpage": "https://odoarchitecture.com/teknopark-istanbul-istanbul-teknoloji-gelistirme-bolgesi-yonetim-ve-ar-ge-yapisi/"
+        }
+    },
+    {
+        "details": {
+            "name": "Bodrum Ticaret Odası",
+            "latLong": [37.05300457309324, 27.38156407093907],
+            "arch": ["Stüdyo 501"],
+            "year": "2015",
+            "webpage": "https://www.studyo501.com/bodrum-t-caret-odasi-h-zmet-b-nasi"
+        }
+    },
+    {
+        "details": {
+            "name": "Diyarbakır Yenişehir Belediye Binası",
+            "latLong": [37.960451505731, 40.18374566086894],
+            "arch": ["Uygur Mimarlık"],
+            "year": "2005-2016",
+            "webpage": "https://uygurmimarlik.com.tr/site/tr/projects/diyarbakir-yenisehir-belediye.html"
+        }
+    },
+    {
+        "details": {
+            "name": "ODTÜ Teknokent Bilişim İnovasyon Merkezi",
+            "latLong": [39.907773413789386, 32.75017606640764],
+            "arch": ["FREA"],
+            "year": "2013",
+            "webpage": "http://frea.com.tr/TR/Projects/41/Secki/276/ODTU-TEKNOKENT-BiLisiM-INOVASYON-MERKEZi/"
+        }
+    },
+    {
+        "details": {
+            "name": "T.C. Merkez Bankası Bursa Şubesi",
+            "latLong": [40.19665592002239, 29.055560365232697],
+            "arch": ["Yalın Mimarlık"],
+            "year": "2005-2011",
+            "webpage": "http://www.yalin-mimarlik.com/projeler/tc-merkez-bankasi"
+        }
+    },
+    {
+        "details": {
+            "name": "Şehit Kamil Sanat Merkezi",
+            "latLong": [37.06616644815365, 37.37225364018447],
+            "arch": ["Kota Mimarlık"],
+            "year": "2016",
+            "webpage": "https://www.kotaarchitects.com/sehitkamil-sanat-merkzei"
+        }
+    },
+    {
+        "details": {
+            "name": "Kayseri Ticaret Odası Hizmet Binası",
+            "latLong": [38.735805875589975, 35.49763623400269],
+            "arch": ["A2 Tasarım"],
+            "year": "2017",
+            "webpage": "https://www.a2tasarim.com/kto"
+        }
+    },
+    {
+        "details": {
+            "name": "İstanbul Deniz Müzesi",
+            "latLong": [41.041721958119425, 29.00575424963969],
+            "arch": ["Teğet Mimarlık"],
+            "year": "2005-2010",
+            "webpage": "https://www.teget.com/works/istanbul-maritime-museum/"
+        }
+    },
+    {
+        "details": {
+            "name": "ODTÜ Lisansüstü Öğrencileri Konukevi",
+            "latLong": [39.88919330899778, 32.77629143425903],
+            "arch": ["Uygur Mimarlık", "Ofis 16 Mimarlık"],
+            "year": "2011-2015",
+            "webpage": "https://www.uygurmimarlik.com.tr/site/tr/projects/odtu-konukevi.html"
+        }
+    },
+    {
+        "details": {
+            "name": "Kayseri Kale İçi Arkeoloji Müzesi",
+            "latLong": [38.72148179621567, 35.487702955774665],
+            "arch": ["Kadir Uyanık", "Zafer Ertürk", "Emel Birer"],
+            "year": "2008-2019",
+            "webpage": "https://odoarchitecture.com/kayseri-kale-ici-arkeoloji-muzesi/"
+        }
+    },
+    {
+        "details": {
+            "name": "ODTÜ KKK Rektörlük, BIM ve Kütüphane Binaları",
+            "latLong": [35.249442335171686, 33.02365219142005],
+            "arch": ["TECE Mimarlık"],
+            "year": "2003-2005",
+            "webpage": "http://www.tecemimarlik.com/project_item/odtu-kktc-kampusu-rektorluk-bim-ve-kutuphane-binalari/"
+        }
+    },
+    {
+        "details": {
+            "name": "İzmir Tasarım Koridorları",
+            "latLong": [38.43877837276971, 27.141428229029366],
+            "arch": ["İki Artı Bir Mimarlık"],
+            "year": "2014-2019",
+            "webpage": "https://odoarchitecture.com/izmir-tasarim-koridorlari-tirtil/"
+        }
+    },
+    {
+        "details": {
+            "name": "Troya Müzesi",
+            "latLong": [39.95545098390391, 26.24940008494366],
+            "arch": ["Yalın Mimarlık"],
+            "year": "2011-2018",
+            "webpage": "http://www.yalin-mimarlik.com/projeler/troya-muzesi"
+        }
+    },
+    {
+        "details": {
+            "name": "Gökçeada Lise Kampüsü",
+            "latLong": [40.20017318855742, 25.903611298260717],
+            "arch": ["PAB Mimarlık"],
+            "year": "2015",
+            "webpage": "https://www.pab.com.tr/gokceada-lise-kampusu"
+        }
+    },
+    {
+        "details": {
+            "name": "Türkan Şoray Kültür Merkezi",
+            "latLong": [36.869904002908555, 30.72504858613458],
+            "arch": ["OfficePAN Mimarlık", "Rasa Mimarlık", "NODE architects"],
+            "year": "2015-2018",
+            "webpage": "https://officepan.com/portfolio/items/cok-amacli-gosteri-merkezi-atolye-ve-sahnesi/"
+        }
+    },
+    {
+        "details": {
+            "name": "Fatma Ana Cemevi ve Kültür Merkezi",
+            "latLong": [40.98064257658791, 28.646223067092127],
+            "arch": ["9016 Mimarlık"],
+            "year": "2015",
+            "webpage": "http://www.dokuzbinonalti.com/tr/project/65"
+        }
+    },
+    {
+        "details": {
+            "name": "Lüleburgaz Yıldızları Kadın Akademisi",
+            "latLong": [41.389545066947726, 27.34891832849572],
+            "arch": ["İki Artı Bir Mimarlık"],
+            "year": "2015-2017",
+            "webpage": "https://odoarchitecture.com/luleburgaz-yildizlari-kadin-akademisi/"
+        }
+    },
+    {
+        "details": {
+            "name": "Lüleburgaz Şehirlerarası Otobüs Terminali",
+            "latLong": [41.404678326584595, 27.347725621482812],
+            "arch": ["kolektif Mimarlar", "Rasa Stüdyo"],
+            "year": "2013-2016",
+            "webpage": "http://kolektifmimarlar.com/projeler/luleburgaz-sehirlerarasi-otobus-terminali-2/"
+        }
+    }
+]
+
+function designerTexter(array) {
+    let designer
+    array.forEach(element => {
+        designer += element
+    })
+    return designer
+}
+projectArray.forEach(element => {
+    let project = element.details
+    L.marker(project.latLong).addTo(map)
+        .bindPopup(project.name + "<br/>" + project.year + "<br/>" + project.arch + "<br/>" + "<a target='_blank' href='" + project.webpage + "'>Proje Sitesi</a>")
+});
